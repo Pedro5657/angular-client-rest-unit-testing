8f7f2288f9684bb4c99e67670e105b4ebb0d3ffa
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-04-23 19:23:32 +0100

    Finished implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './services/user.service';
+import { AsyncPipe } from '@angular/common';
+import { User } from './services/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, AsyncPipe],
+	providers: [UserService],
+  template: `
+		<h1>Users:</h1>
+		<ul>
+			@for(user of users; track user.name) {
+				<li>{{ user.name }}</li>
+			}
+		</ul>
+	`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-client-rest-unit-testing';
+	users: User[] = [];
+
+	constructor(public userService: UserService) {}
+
+	ngOnInit() {
+		this.userService.getAllUsers().subscribe(res => this.users = res);
+	}
 }
