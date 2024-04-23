f94d1a074bdc986794983ffccff6270ed2de08e4
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-04-23 19:30:36 +0100

    Unnecessary imports eliminated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { UserService } from './services/user.service';
-import { AsyncPipe } from '@angular/common';
 import { User } from './services/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe],
 	providers: [UserService],
   template: `
 		<h1>Users:</h1>
